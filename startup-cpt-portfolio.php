34d6a345977c636b60479ad21287e0dabe2ce18e
yozz <yozz@Manava.local>
2015-08-10 14:32:27 -0400

    Import

---
+++ startup-cpt-portfolio.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: StartUp Portfolio Custom Post
+Description: Le plugin pour lactiver le Custom Post Portfolio
+Author: Yann Caplain
+Version: 0.1 alpha
+*/
+
+//CPT
+function startup_reloaded_portfolio() {
+
+	$labels = array(
+		'name'                => _x( 'Portfolio items', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Portfolio item', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Portfolio', 'text_domain' ),
+		'name_admin_bar'      => __( 'Portfolio', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+		'all_items'           => __( 'All Items', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'new_item'            => __( 'New Item', 'text_domain' ),
+		'edit_item'           => __( 'Edit Item', 'text_domain' ),
+		'update_item'         => __( 'Update Item', 'text_domain' ),
+		'view_item'           => __( 'View Item', 'text_domain' ),
+		'search_items'        => __( 'Search Item', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' )
+	);
+	$args = array(
+		'label'               => __( 'portfolio', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'comments', 'trackbacks', 'revisions', 'post-formats' ),
+		'taxonomies'          => array( 'category', 'post_tag' ),
+		'hierarchical'        => true,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-art',
+		'show_in_admin_bar'   => false,
+		'show_in_nav_menus'   => false,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page'
+	);
+	register_post_type( 'portfolio', $args );
+
+}
+add_action( 'init', 'startup_reloaded_portfolio', 0 );
+?>
