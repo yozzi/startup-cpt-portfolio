4ce916906a60d291306e56abae72ab43ef7e9153
yozz <yozz.online@gmail.com>
2016-03-21 09:26:11 -0400

    Minor

--- startup-cpt-portfolio.php
+++ startup-cpt-portfolio.php
@@ -295,7 +295,7 @@ add_shortcode( 'portfolio', 'startup_cpt_portfolio_shortcode' );
 
 // Shortcode UI
 /**
- * Detecion de Shortcake. Identique dans tous les plugins.
+ * Detection de Shortcake. Identique dans tous les plugins.
  */
 if ( !function_exists( 'shortcode_ui_detection' ) ) {
     function shortcode_ui_detection() {
