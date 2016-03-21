9d95ce5564cfc0e3736dd2fbe67f986f7013ea5a
yozz <yozz.online@gmail.com>
2016-03-21 14:37:17 -0400

    Message id template file not found

--- startup-cpt-portfolio.php
+++ startup-cpt-portfolio.php
@@ -287,9 +287,13 @@ function startup_cpt_portfolio_shortcode( $atts ) {
         ), $atts);
     
 	// Code
-        ob_start();
+    ob_start();
+    if ( function_exists( 'startup_reloaded_setup' ) ) {
         require get_template_directory() . '/template-parts/content-portfolio.php';
-        return ob_get_clean();    
+     } else {
+        echo 'Should <a href="https://github.com/yozzi/startup-reloaded" target="_blank">install StartUp Reloaded Theme</a> to make things happen...';
+     }
+     return ob_get_clean();    
 }
 add_shortcode( 'portfolio', 'startup_cpt_portfolio_shortcode' );
 
